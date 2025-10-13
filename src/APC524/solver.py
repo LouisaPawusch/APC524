0d38f15b03389ba6920a07afe1ae1f8a373e74bd
Lara Tobias-Tarsh <lt0663@princeton.edu>
2025-10-13 14:04:46 -0400

    added a dataclass for a basic 2D CA (CGOL)

--- src/APC524/solver.py
+++ src/APC524/solver.py
@@ -1,3 +1,4 @@
+#!/usr/bin/envs python3
 """
 solver.py
 
@@ -20,6 +21,8 @@ TO DO:
 - [ ] Modularise this code more (maybe split the helpers into new file etc)
 - [ ] Further development of the grid function to allow irregular/multidimensional grids
 - [ ] Add more complete, cleaner way of specifying rules aside from the default rules function
+- [ ] Add a run function to the class with a given number of steps (?)
+- [ ] Add a plotting function to the class
 
 """
 # ---------
@@ -150,12 +153,42 @@ def CGOL_rules(grid: np.ndarray, neighbour_counts: np.ndarray) -> np.ndarray:
 # but also want the user to be able to define their own CA within our rules
 
 @dataclasses.dataclass
-class CellularAutomaton2D:
+class CellularAutomaton:
     """
     Class structure for a very basic, 2 dimensional cellular 
     automaton. This structure defaults to the CGOL rules.
     """
-       
+    grid : np.ndarray
+    nstates: int
+    kernel : np.ndarray
+    history: list[np.ndarray] = dataclasses.field(default_factory=list)
+
+    def step(self, rules_fn, convolution_fn):
+        """
+        Function iterates the game in time.
+
+        NOTE: right now I am keeping the convolution function 
+        outside of the class so that we can have a CA that is 
+        3+ dimensional (so that we could do some like simple diffusion
+        model maybe in the atmosphere?)
+
+        Options here - make a CellularAutomaton2D and CellularAutomaton3D
+        separately and make the convolve_neighbours function a class 
+        method OR leave it outside and keep the base class multidimensional?
+
+        Parameters
+        ----------
+        self : CellularAutomaton
+            the cellular automaton object
+        rules_fn : Callable
+            the function which defines the rules and creates a new grid
+        convolution_fn : Callable
+            the function which dictates how to apply the kernel search
+            to the grid
+        """
+        neighbour_counts = convolution_fn(self.grid, self.kernel, self.nstates)
+        self.grid = rules_fn(self.grid, neighbour_counts)
+        self.history.append(self.grid.copy())
 
 
 
